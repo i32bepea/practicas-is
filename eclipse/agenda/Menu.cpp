aabd630f066be79695545a79e4bcc7a6539d9c1b
i32baher <i32baher@uco.es>
2015-01-09 20:38:19 +0100

    Creada función salir() de la clase menu

--- eclipse/agenda/Menu.cpp
+++ eclipse/agenda/Menu.cpp
@@ -12,7 +12,11 @@ void Menu::mostrarMenu(){
 	std::cout<<"1. Insertar contacto.\n2. Buscar contacto.\n3. Borrar contacto.\n4. Modificar contacto.\n5. Mostrar todos los contactos de la agenda.\n6. Seguridad.\n7. Salir.\n";
 
 }
+void Menu::salir(){
 
+	agenda_.volcado(); //Vuelca el contenido de la agenda a agenda.juda
+
+}
 
 Menu::Menu() {
 	// TODO Auto-generated constructor stub
