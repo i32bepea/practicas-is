da98b50cb2c61fb2f8c801eebcc9978f379d798f
i32argaj <i32argaj@uco.es>
2015-01-10 13:04:53 +0100

    Realizando las funciones del menu: borrar y modificar contacto

--- eclipse/agenda/Menu.cpp
+++ eclipse/agenda/Menu.cpp
@@ -17,12 +17,39 @@ void Menu::buscarContacto() {
 	Contacto aux;
 	Agenda a_;
 
+	std::cout << "Introduzca los apellidos de la persona que desea buscar: " << std::endl;
+	std::cin >>aux.getApellidos();
+
+	a_.buscarContacto(aux.getApellidos());
+}
+
+void Menu::borrarContacto() {
+
+	Contacto aux;
+	Agenda a_;
+
+	std::cout << "Introduzca los apellidos de la persona que desea borrar: " << std::endl;
+	std::cin >>aux.getApellidos();
+
+
 
 
 
 
 }
 
+void Menu::modificarContacto() {
+
+	Contacto aux;
+	Agenda a_;
+
+	a_.modificarContacto();
+
+
+
+
+
+}
 
 
 
