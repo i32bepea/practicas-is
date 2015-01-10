35dc9ac7a6d28858b492f03247e0b7edf468f3b9
i32argaj <i32argaj@uco.es>
2015-01-10 12:15:08 +0100

    Realizando las funciones del menu: insertarContacto()

---
+++ eclipse/agenda/Menu.cpp
@@ -0,0 +1,41 @@
+#include "Menu.h"
+#include "Agenda.h"
+#include <iostream>
+
+void Menu::insertarContacto() {
+
+	Contacto aux;
+	Agenda a_;
+
+	//std::cin >> aux;
+
+	a_.insertarContacto(aux);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void Menu::mostrarMenu(){
+
+	std::cout<<"1. Insertar contacto.\n2. Buscar contacto.\n3. Borrar contacto.\n4. Modificar contacto.\n5. Mostrar todos los contactos de la agenda.\n6. Seguridad.\n7. Salir.\n";
+
+}
+
+Menu::Menu() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Menu::~Menu() {
+	// TODO Auto-generated destructor stub
+}
---
+++ eclipse/agenda/Menu.h
@@ -0,0 +1,34 @@
+#ifndef MENU_H_
+#define MENU_H_
+#include "Agenda.h"
+class Menu {
+
+private:
+
+	Agenda a_;
+
+public:
+
+
+	void insertarContacto(); //Se tiene que realizar un cin para rellenar los datos y pasarlo a la función insertarContacto.
+	void buscarContacto(); //Pedirle que introduzca los apellidos del contacto y llamar a la función buscarContacto de agenda.
+	void borrarContacto(); //Pide por pantalla los apellidos, se muestran los contactos encontrados con buscarContacto, y se llama a la función borrarContacto de agenda habiendole pedido antes que introduzca el DNI.
+	void mofificarContacto(); //Llama a la función de modificarContacto de agenda.
+	void listar(); //Tiene que listar todos los contactos y dar la posibilidad de ir hacia atrás.
+	void seguridad(); //Tiene que mostrar el menú de seguridad y llamar a las respectivas funciones.
+	void mostrarMenu(); //Esta función muestra el menú.
+	void salir(); //Esta función se encargará de gestionar la salida de la aplicación, creando el fichero agenda.juda que guardará todos los cambios hechos durante la ejecución. En este caso los cambios están en una lista de contactos y es esta la que hay que añadir y ordenar.
+
+	Menu(); //Tiene que cargar todos los contactos del fichero en la lista de agenda.
+	virtual ~Menu();
+
+	const Agenda& getA() const {
+		return a_;
+	}
+
+	void setA(const Agenda& a) {
+		this->a_ = a;
+	}
+};
+
+#endif /* MENU_H_ */
